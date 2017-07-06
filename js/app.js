ce2ee0f032d75a0a0046f9fce27a6b7e0e2fd81a
Krzysiek91 <domaradzki91@gmail.com>
2017-07-06 03:49:57 +0200

    added click event to the name of the book that toggle slide of book description, edited html (divs instead of table)

--- js/app.js
+++ js/app.js
@@ -3,39 +3,63 @@ $(document).ready(function () {
     books();
 });
 
+var booksListContainer = $('.books-list');
 
-// function to correct
+
 function books() {
 
     $.ajax({
         url: "api/books.php",
         type: 'GET',
-      //  data: "author, name, book_desc",
+        data: "author, name, book_desc",
         dataType: 'JSON',
 
         success: function (data) {
 
-            var booksTable = $('table.books');
-
             console.log(data);
 
-            for (var index in data){
+            for (var index in data) {
 
-                var bookData =
-                    $('<tr>' +
-                    '<td>' + data[index].name + '</td>' +
-                    '<td>' + data[index].author + '</td>' +
-                    '</tr>');
+                // create div with row class and id from DB
+                var id = data[index]['id'];
+                var row = $('<div class="row" data-id="' + id + '">');
 
-                var bookDescription =
-                    $('<tr>' +
-                    '<td>' + data[index].book_desc + '</td>' +
-                    '</tr>');
+                var divColName = $('<div>').addClass('name').addClass('col-xs-4');
+                divColName.text(data[index]['name']);
 
-                booksTable.append(bookData);
-                booksTable.append(bookDescription);
+                var divColAuthor = $('<div>').addClass('author').addClass('col-xs-4');
+                divColAuthor.text(data[index]['author']);
 
+                var divColDescription = $('<div>').addClass('description').addClass('col-sm-8');
+                divColDescription.text(data[index]['book_desc']);
+
+                row.append(divColName);
+                row.append(divColAuthor);
+                row.append(divColDescription);
+
+                booksListContainer.append(row);
             }
         }
     })
+
+    booksListContainer.on('click', 'div.name', function (event) {
+        var bookID = $(this).parent().data('id');
+        var divDesc = $(this).next().next();
+
+        $.ajax({
+            url: "api/books.php?id=" + bookID,
+            type: 'GET',
+            dataType: 'JSON',
+
+            success: function (data) {
+
+                var description = data[bookID]['book_desc'];
+                divDesc.text(description);
+                divDesc.slideToggle();
+            }
+        })
+    });
+
+
 }
+
